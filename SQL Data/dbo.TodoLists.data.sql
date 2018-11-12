c0549f9e48bc7171eede855149c47651f105fc5e
Andrew Conniff <andrew@social-media-productions.com>
2018-11-12 02:35:46 -0800

    update insert queries

--- SQL Data/dbo.TodoLists.data.sql
+++ SQL Data/dbo.TodoLists.data.sql
@@ -1,5 +1,11 @@
-﻿INSERT INTO [dbo].[TodoLists] ([Id], [Name], [Description]) VALUES (N'8912aa35-1433-48fe-ae72-de2aaa38e37d', N'Work Project', N'a list of items for the office')
+﻿INSERT INTO [dbo].[TodoLists] ([Id], [Name], [Description]) VALUES (N'd290f1ee-6c54-4b01-90e6-d701748f0851', N'Home 2', N'The list of things that need to be done at home
+')
+INSERT INTO [dbo].[TodoLists] ([Id], [Name], [Description]) VALUES (N'd290f1ee-6c56-4b01-90e6-d701758f0851', N'Home 2', N'The list of things that need to be done at home
+')
+INSERT INTO [dbo].[TodoLists] ([Id], [Name], [Description]) VALUES (N'd290f1ee-6c88-4b01-90e6-d701758f6851', N'Home 4', N'The list of things that need to be done at home
+')
+INSERT INTO [dbo].[TodoLists] ([Id], [Name], [Description]) VALUES (N'8912aa35-1433-48fe-ae72-de2aaa38e37d', N'Work Project', N'a list of items for the office')
 INSERT INTO [dbo].[TodoLists] ([Id], [Name], [Description]) VALUES (N'8912aa35-1433-48fe-ae72-de2aaa38e37e', N'Home projects', N'A list of work ites for the home')
 INSERT INTO [dbo].[TodoLists] ([Id], [Name], [Description]) VALUES (N'8912aa35-1433-48fe-ae72-de2aaa38e37f', N'Fun Projects', N'A list of items that are fun')
-INSERT INTO [dbo].[TodoLists] ([Id], [Name], [Description]) VALUES (N'8912aa35-1433-48fe-ae72-de2aaa38e88a', N'Ambers work', N'a list of projects for Amber')
 INSERT INTO [dbo].[TodoLists] ([Id], [Name], [Description]) VALUES (N'8912aa35-1433-48fe-ae72-de2aaa38e44a', N'Andrew list', N'A list of projects for Andrew')
+INSERT INTO [dbo].[TodoLists] ([Id], [Name], [Description]) VALUES (N'8912aa35-1433-48fe-ae72-de2aaa38e88a', N'Ambers work', N'a list of projects for Amber')
